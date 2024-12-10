pve-loong64-port/proxmox-backup

commit 668b8383a7b78e79ec6e59b6baa7517ecc6a90bc

file restore: qemu helper: switch to more modern blockdev option for drives
From the QEMU man page:

> The most explicit way to describe disks is to use a combination of
> -device to specify the hardware device and -blockdev to describe the
> backend. The device defines what the guest sees and the backend
> describes how QEMU handles the data. It is the only guaranteed stable
> interface for describing block devices and as such is recommended for
> management tools and scripting.

> The -drive option combines the device and backend into a single
> command line option which is a more human friendly. There is however
> no interface stability guarantee although some older board models
> still need updating to work with the modern blockdev forms.

From the perspective of live restore, there should be no behavioral
change, except that the used driver is now explicitly specified. The
'-device' options are still the same, the fact that 'if=none' is gone
shouldn't matter, because the '-device' option was already used to
define the interface (i.e. virito-blk) and the 'id' option needed to
be replaced with 'node-name'.

Signed-off-by: Fiona Ebner <f.ebner@proxmox.com>

proxmox-file-restore/src/qemu_helper.rs
@@ -279,7 +279,7 @@ pub async fn start_vm(
         if !file.ends_with(".img.fidx") {
             continue;
         }
-        drives.push("-drive".to_owned());
+        drives.push("-blockdev".to_owned());
         let keyfile = if let Some(ref keyfile) = details.keyfile {
             format!(",,keyfile={keyfile}")
         } else {
@@ -291,7 +291,7 @@ pub async fn start_vm(
             format!(",,namespace={}", details.namespace)
         };
         drives.push(format!(
-            "file=pbs:repository={}{},,snapshot={},,archive={}{},read-only=on,if=none,id=drive{}",
+            "driver=pbs,repository={}{},snapshot={},archive={}{},read-only=on,node-name=drive{}",
             details.repo, namespace, details.snapshot, file, keyfile, id
         ));
 

commit 34fbf1a809251da97a471fb07b2b0a84bdbd6ef9

file-restore: fix -blockdev regression with namespaces or encryption
QEMU CLI option parsing requires doubling the commas for values, this
seems to be also used when a combined option is used to pass down the
key=value pairs to the internal options, like for the combined -drive
option that was replaced by the slightly lower-level blockdev option
in commit 668b8383 ("file restore: qemu helper: switch to more modern
blockdev option for drives"). So there we now could drop the comma
duplication as blockdev directly interprets these options, thus no
need for escaping the comma.

We missed two instances because they were not part of the "main"
format string, which broke some use cases.

Fixes: 668b8383 ("file restore: qemu helper: switch to more modern blockdev option for drives")
Signed-off-by: Fabian Grünbichler <f.gruenbichler@proxmox.com>
Tested-by: Mira Limbeck <m.limbeck@proxmox.com>
 [ TL: add more context, but it's a bit guesstimation ]
Signed-off-by: Thomas Lamprecht <t.lamprecht@proxmox.com>

proxmox-file-restore/src/qemu_helper.rs
@@ -281,14 +281,14 @@ pub async fn start_vm(
         }
         drives.push("-blockdev".to_owned());
         let keyfile = if let Some(ref keyfile) = details.keyfile {
-            format!(",,keyfile={keyfile}")
+            format!(",keyfile={keyfile}")
         } else {
             "".to_owned()
         };
         let namespace = if details.namespace.is_root() {
             String::new()
         } else {
-            format!(",,namespace={}", details.namespace)
+            format!(",namespace={}", details.namespace)
         };
         drives.push(format!(
             "driver=pbs,repository={}{},snapshot={},archive={}{},read-only=on,node-name=drive{}",