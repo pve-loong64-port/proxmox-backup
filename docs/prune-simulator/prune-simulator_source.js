pve-loong64-port/proxmox-backup

commit bb044304aafdbbf0e4e0c819904ca06c1a91848b

prune sim: move PruneList to more static declaration
Signed-off-by: Thomas Lamprecht <t.lamprecht@proxmox.com>

docs/prune-simulator/prune-simulator_source.js
@@ -109,63 +109,61 @@ Ext.onReady(function() {
 	extend: 'Ext.panel.Panel',
 	alias: 'widget.prunesimulatorPruneList',
 
+	viewModel: {},
+
+	items: [{
+	    xtype: 'grid',
+	    bind: {
+		store: '{store}',
+	    },
+	    border: false,
+	    columns: [
+		{
+		    header: 'Backup Time',
+		    dataIndex: 'backuptime',
+		    renderer: function(value, metaData, { data }) {
+			let text = Ext.Date.format(value, 'Y-m-d H:i:s');
+			if (data.mark !== 'keep') {
+			    return `<div style="text-decoration: line-through;">${text}</div>`;
+			}
+			if (me.useColors) {
+			    let bgColor = COLORS[data.keepName];
+			    let textColor = TEXT_COLORS[data.keepName];
+			    return `<div style="background-color: ${bgColor};color: ${textColor};">${text}</div>`;
+			} else {
+			    return text;
+			}
+		    },
+		    flex: 1,
+		    sortable: false,
+		},
+		{
+		    header: 'Keep (reason)',
+		    dataIndex: 'mark',
+		    renderer: function(value, metaData, { data }) {
+			if (data.mark !== 'keep') {
+			    return value;
+			}
+			if (data.keepCount) {
+			    return `keep (${data.keepName}: ${data.keepCount})`;
+			} else {
+			    return `keep (${data.keepName})`;
+			}
+		    },
+		    width: 200,
+		    sortable: false,
+		},
+	    ],
+	}],
+
 	initComponent: function() {
 	    let me = this;
 
 	    if (!me.store) {
 		throw "no store specified";
 	    }
-
-	    me.items = [
-		{
-		    xtype: 'grid',
-		    store: me.store,
-		    border: false,
-		    columns: [
-			{
-			    header: 'Backup Time',
-			    dataIndex: 'backuptime',
-			    renderer: function(value, metaData, record) {
-				let text = Ext.Date.format(value, 'Y-m-d H:i:s');
-				if (record.data.mark === 'keep') {
-				    if (me.useColors) {
-					let bgColor = COLORS[record.data.keepName];
-					let textColor = TEXT_COLORS[record.data.keepName];
-					return '<div style="background-color: ' + bgColor + '; ' +
-							    'color: ' + textColor + ';">' + text + '</div>';
-				    } else {
-					return text;
-				    }
-				} else {
-				    return '<div style="text-decoration: line-through;">' + text + '</div>';
-				}
-			    },
-			    flex: 1,
-			    sortable: false,
-			},
-			{
-			    header: 'Keep (reason)',
-			    dataIndex: 'mark',
-			    renderer: function(value, metaData, record) {
-				if (record.data.mark === 'keep') {
-				    if (record.data.keepCount) {
-					return 'keep (' + record.data.keepName +
-					       ': ' + record.data.keepCount + ')';
-				    } else {
-					return 'keep (' + record.data.keepName + ')';
-				    }
-				} else {
-				    return value;
-				}
-			    },
-			    width: 200,
-			    sortable: false,
-			},
-		    ],
-		},
-	    ];
-
 	    me.callParent();
+	    me.getViewModel().set('store', me.store);
 	},
     });
 
@@ -222,8 +220,7 @@ Ext.onReady(function() {
 			    if (me.useColors) {
 				let bgColor = COLORS[backup.data.keepName];
 				let textColor = TEXT_COLORS[backup.data.keepName];
-				html += `<span style="background-color: ${bgColor};
-				    color: ${textColor};">${text}</span>`;
+				html += `<span style="background-color: ${bgColor}; color: ${textColor};">${text}</span>`;
 			    } else {
 				html += `<span class="black">${text}</span>`;
 			    }

commit 41588772c945287a2ee145ab78ac4f95a1ac0ba3

fix: docs: prune sim: show "keep" entries in backup list
Currently, the list of backups only shows removed backups and is
missing backups that are kept, though they are shown correctly in the
calendar view.

The reason is that a refactor (see Fixes tag) moved the definition of
a custom field renderer referencing `me` to a scope where `me` is not
defined. This causes the renderer to error out for "kept" backups,
which apparently causes the grid to skip the rows altogether (without
any messages in the console).

Fix this by replacing the broken `me` reference.

Fixes: bb044304 ("prune sim: move PruneList to more static declaration")
Signed-off-by: Friedrich Weber <f.weber@proxmox.com>

docs/prune-simulator/prune-simulator_source.js
@@ -126,7 +126,8 @@ Ext.onReady(function() {
 			if (data.mark !== 'keep') {
 			    return `<div style="text-decoration: line-through;">${text}</div>`;
 			}
-			if (me.useColors) {
+			let pruneList = this.up('prunesimulatorPruneList');
+			if (pruneList.useColors) {
 			    let bgColor = COLORS[data.keepName];
 			    let textColor = TEXT_COLORS[data.keepName];
 			    return `<div style="background-color: ${bgColor};color: ${textColor};">${text}</div>`;