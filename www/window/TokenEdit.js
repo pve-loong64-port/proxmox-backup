pve-loong64-port/proxmox-backup

commit 3fdf8769f45ef3412218db7a600ab7273ccbbd6a

api: token: make comment deletable
Currently, the only way to delete a comment on a token is to set it to
just spaces. Since we trim it in the endpoint, it gets deleted as a
side effect. This allows the comment to be deleted properly.

Signed-off-by: Hannes Laimer <h.laimer@proxmox.com>

www/window/TokenEdit.js
@@ -68,6 +68,7 @@ Ext.define('PBS.window.TokenEdit', {
 	    {
 		xtype: 'proxmoxtextfield',
 		name: 'comment',
+		deleteEmpty: true,
 		fieldLabel: gettext('Comment'),
 	    },
 	],

src/api2/access/user.rs
@@ -515,6 +515,15 @@ pub fn generate_token(
     }))
 }
 
+#[api()]
+#[derive(Serialize, Deserialize)]
+#[serde(rename_all = "kebab-case")]
+/// The set of properties that can be deleted from a token.
+pub enum DeletableTokenProperty {
+    /// Delete the comment property.
+    Comment,
+}
+
 #[api(
     protected: true,
     input: {
@@ -537,6 +546,14 @@ pub fn generate_token(
                 schema: EXPIRE_USER_SCHEMA,
                 optional: true,
             },
+            delete: {
+                description: "List of properties to delete.",
+                type: Array,
+                optional: true,
+                items: {
+                    type: DeletableTokenProperty,
+                }
+            },
             digest: {
                 optional: true,
                 schema: PROXMOX_CONFIG_DIGEST_SCHEMA,
@@ -557,6 +574,7 @@ pub fn update_token(
     comment: Option<String>,
     enable: Option<bool>,
     expire: Option<i64>,
+    delete: Option<Vec<DeletableTokenProperty>>,
     digest: Option<String>,
 ) -> Result<(), Error> {
     let _lock = pbs_config::user::lock_config()?;
@@ -572,6 +590,14 @@ pub fn update_token(
     let tokenid_string = tokenid.to_string();
 
     let mut data: ApiToken = config.lookup("token", &tokenid_string)?;
+
+    if let Some(delete) = delete {
+        for delete_prop in delete {
+            match delete_prop {
+                DeletableTokenProperty::Comment => data.comment = None,
+            }
+        }
+    }
 
     if let Some(comment) = comment {
         let comment = comment.trim().to_string();

commit d49a27ede88b1f87ab6bcf07d8e55694eb414124

ui: only add delete parameter on token edit, not when creating tokens
otherwise tokens without comments can no longer be created as the api
will reject the additional `delete` parameter. this bug was introduced
by commit:

3fdf876: api: token: make comment deletable
Signed-off-by: Shannon Sterz <s.sterz@proxmox.com>

www/window/TokenEdit.js
@@ -68,8 +68,10 @@ Ext.define('PBS.window.TokenEdit', {
 	    {
 		xtype: 'proxmoxtextfield',
 		name: 'comment',
-		deleteEmpty: true,
 		fieldLabel: gettext('Comment'),
+		cbind: {
+		    deleteEmpty: "{!isCreate}"
+		},
 	    },
 	],
     },