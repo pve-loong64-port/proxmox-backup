pve-loong64-port/proxmox-backup

commit d49a27ede88b1f87ab6bcf07d8e55694eb414124

ui: only add delete parameter on token edit, not when creating tokens
otherwise tokens without comments can no longer be created as the api
will reject the additional `delete` parameter. this bug was introduced
by commit:

3fdf876: api: token: make comment deletable
Signed-off-by: Shannon Sterz <s.sterz@proxmox.com>

www/window/TokenEdit.js
@@ -68,8 +68,10 @@ Ext.define('PBS.window.TokenEdit', {
 	    {
 		xtype: 'proxmoxtextfield',
 		name: 'comment',
-		deleteEmpty: true,
 		fieldLabel: gettext('Comment'),
+		cbind: {
+		    deleteEmpty: "{!isCreate}"
+		},
 	    },
 	],
     },

commit dc914094c90b853141edd80470186654b4700669

ui: token edit: fix missing trailing-comma
Fixes: d49a27ed ("ui: only add delete parameter on token edit, not when creating tokens")
Signed-off-by: Thomas Lamprecht <t.lamprecht@proxmox.com>

www/window/TokenEdit.js
@@ -70,7 +70,7 @@ Ext.define('PBS.window.TokenEdit', {
 		name: 'comment',
 		fieldLabel: gettext('Comment'),
 		cbind: {
-		    deleteEmpty: "{!isCreate}"
+		    deleteEmpty: "{!isCreate}",
 		},
 	    },
 	],