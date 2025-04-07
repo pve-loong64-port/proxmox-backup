pve-loong64-port/proxmox-backup

commit 5e778d983ab431258007b76ad5114d607c1ac890

ui: datastore tuning options: increase width and rework labels
This was getting cramped, and while it might be actually even nicer to
got to more verbose style like we use for advanced settings of backup
jobs in Proxmox VE, with actual sentences describing the options basic
effects and rationale.

But this is way quicker to do and adds already a bit more rationale,
and we can always do more later on when there's less release time
pressure.

Signed-off-by: Thomas Lamprecht <t.lamprecht@proxmox.com>

www/datastore/OptionView.js
@@ -236,7 +236,7 @@ Ext.define('PBS.Datastore.Options', {
 		xtype: 'proxmoxWindowEdit',
 		title: gettext('Tuning Options'),
 		onlineHelp: 'datastore_tuning_options',
-		width: 350,
+		width: 500,
 		items: {
 		    xtype: 'inputpanel',
 		    onGetValues: function(values) {
@@ -258,7 +258,8 @@ Ext.define('PBS.Datastore.Options', {
 			{
 			    xtype: 'proxmoxKVComboBox',
 			    name: 'chunk-order',
-			    fieldLabel: gettext('Chunk Order'),
+			    fieldLabel: gettext('Chunk Iteration Order'),
+			    labelWidth: 200,
 			    comboItems: Object.entries(PBS.Utils.tuningOptions['chunk-order']),
 			    deleteEmpty: true,
 			    value: '__default__',
@@ -266,7 +267,8 @@ Ext.define('PBS.Datastore.Options', {
 			{
 			    xtype: 'proxmoxKVComboBox',
 			    name: 'sync-level',
-			    fieldLabel: gettext('Sync Level'),
+			    fieldLabel: gettext('Data Sync Level'),
+			    labelWidth: 200,
 			    comboItems: Object.entries(PBS.Utils.tuningOptions['sync-level']),
 			    deleteEmpty: true,
 			    value: '__default__',
@@ -274,7 +276,8 @@ Ext.define('PBS.Datastore.Options', {
 			{
 			    xtype: 'proxmoxcheckbox',
 			    name: 'gc-atime-safety-check',
-			    fieldLabel: gettext('GC atime Check'),
+			    fieldLabel: gettext('GC Access-Time Support Check'),
+			    labelWidth: 200,
 			    autoEl: {
 				tag: 'div',
 				'data-qtip': gettext('Ensure underlying storage honors access time updates'),
@@ -287,19 +290,17 @@ Ext.define('PBS.Datastore.Options', {
 			{
 			    xtype: 'proxmoxintegerfield',
 			    name: 'gc-atime-cutoff',
+			    fieldLabel: gettext('GC Access-Time Cutoff (minutes)'),
+			    labelWidth: 200,
 			    emptyText: gettext('1445 (24 hours 5 minutes)'),
-			    fieldLabel: gettext('GC atime Cutoff'),
-			    autoEl: {
-				tag: 'div',
-				'data-qtip': gettext('Cutoff for atime in minutes'),
-			    },
 			    deleteEmpty: true,
 			},
 			{
 			    xtype: 'proxmoxintegerfield',
 			    name: 'gc-cache-capacity',
-			    fieldLabel: gettext('GC cache capacity'),
-			    emptyText: Proxmox.Utils.defaultText,
+			    fieldLabel: gettext('GC Cache Capacity (# chunks)'),
+			    labelWidth: 200,
+			    emptyText: gettext('1048576 (0 disables chache)'),
 			    minValue: 0,
 			    maxValue: 8 * 1024 * 1024,
 			    deleteEmpty: true,

www/Utils.js
@@ -851,12 +851,12 @@ Ext.define('PBS.Utils', {
 
 	let gc_atime_safety_check = tuning['gc-atime-safety-check'];
 	delete tuning['gc-atime-safety-check'];
-	options.push(`${gettext('GC Access Time Safety Check')}: ${gc_atime_safety_check ?? true}`);
+	options.push(`${gettext('GC Access-Time Support Check')}: ${gc_atime_safety_check ?? true}`);
 
 	let gc_atime_cutoff = tuning['gc-atime-cutoff'];
 	delete tuning['gc-atime-cutoff'];
 	gc_atime_cutoff = gc_atime_cutoff ?? '1445';
-	options.push(`${gettext('GC Access Time Cutoff')}: ${gc_atime_cutoff}m`);
+	options.push(`${gettext('GC Access-Time Cutoff')}: ${gc_atime_cutoff}m`);
 
 	let gc_cache_capacity = tuning['gc-cache-capacity'];
 	delete tuning['gc-cache-capacity'];

commit 33024ffd4384b11f128493e06ec537d1028431fe

options-view: Fix typo in chache
Fixes: 5e778d98 ("ui: datastore tuning options: increase width and rework labels")
Signed-off-by: Maximiliano Sandoval <m.sandoval@proxmox.com>
Link: https://lore.proxmox.com/20250407134039.383887-1-m.sandoval@proxmox.com

www/datastore/OptionView.js
@@ -300,7 +300,7 @@ Ext.define('PBS.Datastore.Options', {
 			    name: 'gc-cache-capacity',
 			    fieldLabel: gettext('GC Cache Capacity (# chunks)'),
 			    labelWidth: 200,
-			    emptyText: gettext('1048576 (0 disables chache)'),
+			    emptyText: gettext('1048576 (0 disables cache)'),
 			    minValue: 0,
 			    maxValue: 8 * 1024 * 1024,
 			    deleteEmpty: true,