pve-loong64-port/proxmox-backup

commit 760f40de97f0dc985ff3ef7e58b13e27ea9d041a

api: verify fixed index writer size on close
Compare it to the value submitted by the client to check that resizing
worked as expected.

Signed-off-by: Robert Obkircher <r.obkircher@proxmox.com>
Reviewed-by: Christian Ebner <c.ebner@proxmox.com>
Tested-by: Christian Ebner <c.ebner@proxmox.com>
Link: https://lore.proxmox.com/all/20260210150642.469670-12-r.obkircher@proxmox.com

src/api2/backup/environment.rs
@@ -630,6 +630,14 @@ impl BackupEnvironment {
             }
         }
 
+        let writer_size = data.index.size();
+        if size != writer_size {
+            bail!(
+                "fixed writer '{}' close failed - unexpected size ({size} != {writer_size})",
+                data.name,
+            );
+        }
+
         let expected_csum = data.index.close()?;
         data.closed = true;
 

pbs-datastore/src/fixed_index.rs
@@ -424,6 +424,11 @@ impl FixedIndexWriter {
         self.index_length
     }
 
+    /// The current total size of the referenced content.
+    pub fn size(&self) -> u64 {
+        self.size
+    }
+
     fn unmap(&mut self) -> Result<(), Error> {
         let Some(index) = NonNull::new(self.index as *mut std::ffi::c_void) else {
             return Ok(());

commit 09ca2b578bc446cd54444e903cd4ba42b3d2968b

api: backup: don't verify total file size for incremental backups
This fixes incremental qemu backups, where the submitted value is the
upload size instead of the total size.

Avoiding the check is preferable over breaking backwards compatability
of the API because it was only relevant for detecting potential bugs.

Signed-off-by: Robert Obkircher <r.obkircher@proxmox.com>
Fixes: 760f40de ("api: verify fixed index writer size on close")
Reviewed-by: Christian Ebner <c.ebner@proxmox.com>
Tested-by: Christian Ebner <c.ebner@proxmox.com>
Tested-by: Stoiko Ivanov <s.ivanov@proxmox.com>
Link: https://lore.proxmox.com/20260310160537.182153-2-r.obkircher@proxmox.com
Signed-off-by: Thomas Lamprecht <t.lamprecht@proxmox.com>

src/api2/backup/environment.rs
@@ -632,14 +632,14 @@ impl BackupEnvironment {
                     );
                 }
             }
-        }
 
-        let writer_size = data.index.size();
-        if size != writer_size {
-            bail!(
-                "fixed writer '{}' close failed - unexpected size ({size} != {writer_size})",
-                data.name,
-            );
+            let writer_size = data.index.size();
+            if size != writer_size {
+                bail!(
+                    "fixed writer '{}' close failed - unexpected size ({size} != {writer_size})",
+                    data.name,
+                );
+            }
         }
 
         let expected_csum = data.index.close()?;