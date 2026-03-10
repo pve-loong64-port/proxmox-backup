pve-loong64-port/proxmox-backup

commit 28fb8143bb85d3cfe3430040259283c413f6495a

api: backup: make fixed index file size optional
Pass the optional size to the FixedIndexWriter to make it grow as
necessary.

Signed-off-by: Robert Obkircher <r.obkircher@proxmox.com>
Reviewed-by: Christian Ebner <c.ebner@proxmox.com>
Tested-by: Christian Ebner <c.ebner@proxmox.com>
Link: https://lore.proxmox.com/all/20260210150642.469670-11-r.obkircher@proxmox.com

src/api2/backup/mod.rs
@@ -456,7 +456,7 @@ pub const API_METHOD_CREATE_FIXED_INDEX: ApiMethod = ApiMethod::new(
             ("archive-name", false, &BACKUP_ARCHIVE_NAME_SCHEMA),
             (
                 "size",
-                false,
+                true,
                 &IntegerSchema::new("File size.").minimum(1).schema()
             ),
             (
@@ -480,7 +480,7 @@ fn create_fixed_index(
     let env: &BackupEnvironment = rpcenv.as_ref();
 
     let name = required_string_param(&param, "archive-name")?.to_owned();
-    let size = required_integer_param(&param, "size")? as u64;
+    let size = param["size"].as_u64();
     let reuse_csum = param["reuse-csum"].as_str();
 
     let archive_name = name.clone();
@@ -528,9 +528,7 @@ fn create_fixed_index(
         reader = Some(index);
     }
 
-    let mut writer = env
-        .datastore
-        .create_fixed_writer(&path, Some(size), chunk_size)?;
+    let mut writer = env.datastore.create_fixed_writer(&path, size, chunk_size)?;
 
     if let Some(reader) = reader {
         writer.clone_data_from(&reader)?;

src/api2/backup/environment.rs
@@ -67,7 +67,7 @@ struct DynamicWriterState {
 struct FixedWriterState {
     name: String,
     index: FixedIndexWriter,
-    size: u64,
+    size: Option<u64>,
     chunk_size: u32,
     chunk_count: u64,
     small_chunk_count: usize, // allow 0..1 small chunks (last chunk may be smaller)
@@ -349,7 +349,7 @@ impl BackupEnvironment {
         &self,
         index: FixedIndexWriter,
         name: String,
-        size: u64,
+        size: Option<u64>,
         chunk_size: u32,
         incremental: bool,
     ) -> Result<usize, Error> {
@@ -618,13 +618,15 @@ impl BackupEnvironment {
                 );
             }
 
-            if size != data.size {
-                bail!(
-                    "fixed writer '{}' close failed - unexpected file size ({} != {})",
-                    data.name,
-                    data.size,
-                    size
-                );
+            if let Some(known_size) = data.size {
+                if size != known_size {
+                    bail!(
+                        "fixed writer '{}' close failed - unexpected file size ({} != {})",
+                        data.name,
+                        known_size,
+                        size,
+                    );
+                }
             }
         }
 

commit 389bcff4a6641ccbc071f4ed7c3dd5ffbcc4bd5d

api: backup: disallow incremental backups with different index length
Restore the previous restrictions because fixed index files are only
reused for qemu backups where the total size never changes.

Signed-off-by: Robert Obkircher <r.obkircher@proxmox.com>
Fixes: 28fb8143 ("api: backup: make fixed index file size optional")
Reviewed-by: Christian Ebner <c.ebner@proxmox.com>
Tested-by: Christian Ebner <c.ebner@proxmox.com>
Tested-by: Stoiko Ivanov <s.ivanov@proxmox.com>
Link: https://lore.proxmox.com/20260310160537.182153-4-r.obkircher@proxmox.com
 [TL: fix "compatibility" typo spotted by Chris]
Signed-off-by: Thomas Lamprecht <t.lamprecht@proxmox.com>

src/api2/backup/mod.rs
@@ -498,6 +498,11 @@ fn create_fixed_index(
     let mut incremental = false;
     if let Some(csum) = reuse_csum {
         incremental = true;
+
+        if size.is_none() {
+            bail!("size is required for incremental backups");
+        }
+
         let last_backup = match &env.last_backup {
             Some(info) => info,
             None => {
@@ -531,6 +536,14 @@ fn create_fixed_index(
     let mut writer = env.datastore.create_fixed_writer(&path, size, chunk_size)?;
 
     if let Some(reader) = reader {
+        // compares chunk count instead of size for backwards compatibility
+        if writer.index_length() != reader.index_count() {
+            bail!(
+                "cannot reuse index - index sizes not equal ({} != {})",
+                writer.index_length(),
+                reader.index_count()
+            );
+        }
         writer.clone_data_from(&reader)?;
     }
 